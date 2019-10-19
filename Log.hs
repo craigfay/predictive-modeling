1ef4df5c8ece0364419dc19e271f337ac09b31af
Craig Fay <craigfay1@gmail.com>
2019-10-19 10:47:12 -0500

    Use cases of Maybe and Either

--- Log.hs
+++ Log.hs
@@ -1,5 +1,15 @@
--- This version of the logarithm function will always remind you to handle
--- the NaN operation
-safeLog x
+-- Two commonly used standard types
+data Maybe a = Nothing | Just a
+
+data Either a b = Left a | Right b 
+
+-- These logarithm functions will always remind you to handle the NaN operation
+-- They're use cases for the Maybe and Either types respsectively
+
+safeLogUsingMaybe x
     | x > 0.0 = Just (log x)
     | otherwise = Nothing
+
+safeLogUsingEither x
+    | x > 0.0   = Right (log x)
+    | otherwise = Left  "Logarithm argument must be positive!"
